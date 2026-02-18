ca8a8b377b8202b36538ba0aef768ca4113c0855
balarammukhi245-create <balarammukhi245@gmail.com>
2026-02-18 17:46:04 +0530

    feat: add updateAccountDetails functionality to update user fullname and email

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -215,7 +215,7 @@ const refreshAccessToken = asyncHandler( async (req, res)=>{
 const changeCurrentPassword = asyncHandler(async(req,res)=>{
     const {oldPassword, newPassword} = req.body
     
-    const user = await  User.findById(req.user?.id)
+    const user = await  User.findById(req.user?._id)
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
 
     if (!isPasswordCorrect){
@@ -237,6 +237,29 @@ const getCurrentUser = asyncHandler(async(req,res)=>{
     .json(200, req.user, "Curresnt user fetched successfully")
 })
 
+const updateAccountDetails = asyncHandler(async (req,res)=>{
+    const {fullname, email} = req.body
+
+    if (!fullname || !email){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullname: fullname,
+                email: email
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details update successfully"))
+})
+
 export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword,
-    getCurrentUser
+    getCurrentUser, updateAccountDetails
 }
